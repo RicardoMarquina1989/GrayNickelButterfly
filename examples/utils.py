0a9b36e3e711de2b743c1828bf9b90496218c06a
ricardomarquina1989 <ricardoandresmarquinamolina92@gmail.com>
2024-04-02 17:00:58 -0300

    update get_context function

--- examples/utils.py
+++ examples/utils.py
@@ -8,18 +8,12 @@ from orca_whirlpool.constants import ORCA_WHIRLPOOL_PROGRAM_ID
 from orca_whirlpool.context import WhirlpoolContext
 
 from constants import *
-from orca_whirlpools_py.position_manager import open_position
-gctx = None
 
-def get_context() -> WhirlpoolContext:
-    global gctx
-    if gctx is not None:
-        return gctx
-    
-    """
-        Read wallet from file and return the keypair.
-        Select one wallet based on APP_ENV.
-    """
+"""
+    Read wallet from file and return the keypair.
+    Select one wallet based on APP_ENV.
+"""
+def get_context() -> WhirlpoolContext:   
     wallet_path = ("wallet.json", "wallet_main.json")[APP_ENV == "main"]
     with Path(wallet_path).open() as f:
         keypair = Keypair.from_bytes(bytes(json.load(f)))
@@ -27,5 +21,4 @@ def get_context() -> WhirlpoolContext:
 
     connection = AsyncClient(RPC_ENDPOINT_URL)  # Create Solana RPC client
     ctx = WhirlpoolContext(ORCA_WHIRLPOOL_PROGRAM_ID, connection, keypair)  # Create Whirlpool context
-    gctx = ctx
-    return gctx
+    return ctx
