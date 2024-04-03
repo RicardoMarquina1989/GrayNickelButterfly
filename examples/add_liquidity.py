c06e0b116dc02db2ddfd198f401ad06d30b36f94
ricardomarquina1989 <ricardoandresmarquinamolina92@gmail.com>
2024-04-02 23:48:41 -0300

    complete add liquidity

---
+++ examples/add_liquidity.py
@@ -0,0 +1,31 @@
+import argparse
+import asyncio
+import sys
+
+from solders.pubkey import Pubkey
+
+from constants import SOL_USDC_WHIRLPOOL_PUBKEY
+from orca_whirlpools_py.position_manager import add_liquidity
+from utils import get_context
+'''
+Open a position considering various options.
+Command sample
+python add_liquidity.py -P 5ebE8hm4g2scC7w7KQGP8z22hMDdkShWVCgE3MHBFSLE -s 0.3 -S 0 -D 0.001
+'''
+async def main():
+    parser = argparse.ArgumentParser(description="Open a position in the specific whirlpool.")
+    parser.add_argument("-P", "--position_pubkey", help="Whirlpool public key", default=SOL_USDC_WHIRLPOOL_PUBKEY)
+    parser.add_argument('-s', "--slippage", type=float, help="acceptable slippage value when deposit", default=0.3)
+    parser.add_argument('-S', "--priority_fee", type=float, help="Priority fee(0~1000)", default=0)
+    parser.add_argument('-D', "--deposit_amount", type=float, help="Deposit amount when increase liquidity")
+    args = parser.parse_args()
+
+    if args.position_pubkey is None or args.deposit_amount is None :
+        parser.error("Invalid input parameters. Please check position_pubkey and deposit_amount value.")
+        sys.exit(1)
+    
+    ctx = get_context()
+    position_pubkey = Pubkey.from_string(args.position_pubkey)
+    await add_liquidity(ctx=ctx, position_pubkey=position_pubkey, slippage=args.slippage, priority_fee=0, deposit_amount='0.01')
+        
+asyncio.run(main())
--- src/orca_whirlpools_py/position_manager.py
+++ src/orca_whirlpools_py/position_manager.py
@@ -15,12 +15,15 @@ def rand_pubkey() -> Pubkey:
 
 
 """
-@func:      Open a position based on upper and lower parameters.
+@func:      Open a position     based on upper and lower parameters.
 @param:     WhirlpoolContext    ctx
-@param:     int             priority_fee 1000>priority>=0
+@param:     int                 priority_fee unit-lamport
 @return:    
 """
-async def open_position(ctx: WhirlpoolContext, whirlpool_pubkey: Pubkey, upper: float, lower: float, deposit_amount: float, slippage:int=30, priority_fee: float = 0):
+async def open_position(ctx: WhirlpoolContext, whirlpool_pubkey: Pubkey, 
+                        upper: float, lower: float, deposit_amount: float,
+                        slippage:int=30, priority_fee: int = 0):
+    
     # get whirlpool
     whirlpool = await ctx.fetcher.get_whirlpool(whirlpool_pubkey)
     decimals_a = (await ctx.fetcher.get_token_mint(whirlpool.token_mint_a)).decimals  # SOL_DECIMAL
@@ -117,7 +120,11 @@ async def open_position(ctx: WhirlpoolContext, whirlpool_pubkey: Pubkey, upper: 
         )
     )
     tx.add_instruction(increase_liquidity_ix)
-
+    if priority_fee is not None:
+        tx.set_compute_unit_limit(200000)
+        # add priority fees (+ 1000 lamports)
+        # tx.set_compute_unit_price(int(1000 * 10**6 / 200000))
+        tx.set_compute_unit_price(int(priority_fee * 10**6 / 200000))
     # execute
     signature = await tx.build_and_execute()
     print("TX signature", signature)
@@ -171,3 +178,128 @@ async def open_position_only(ctx:WhirlpoolContext, whirlpool_pubkey: Pubkey, upp
     # Execute transaction
     signature = await tx.build_and_execute()
     print("TX signature", signature)
+
+"""
+@func:      Add liquidity to the specified position(select position address)
+@param:     WhirlpoolContext    ctx
+@param:     Pubkey              position_pubkey
+@param:     int                 priority_fee unit-lamport
+@return:    
+"""
+async def add_liquidity(ctx: WhirlpoolContext, position_pubkey: Pubkey, deposit_amount: float, slippage:int=30, priority_fee: int = 0):
+    output = {}
+    position_pda = PDAUtil.get_position(ctx.program_id, position_pubkey).pubkey
+    position = await ctx.fetcher.get_position(position_pda, True)
+    # print(position)
+    whirlpool_pubkey = position.whirlpool
+    # get whirlpool
+    print('{:<80}'.format("Getting whirlpool info..."))        
+    whirlpool = await ctx.fetcher.get_whirlpool(whirlpool_pubkey)
+    decimals_a = (await ctx.fetcher.get_token_mint(whirlpool.token_mint_a)).decimals  # SOL_DECIMAL
+    decimals_b = (await ctx.fetcher.get_token_mint(whirlpool.token_mint_b)).decimals  # USDC_DECIMAL
+    price = PriceMath.sqrt_price_x64_to_price(whirlpool.sqrt_price, decimals_a, decimals_b)
+    print('{:*^80}'.format("Whirlpool"))
+    print('{:>20} {}'.format("whirlpool:", position.whirlpool))
+    print('{:>20} {}'.format("token_mint_a:", whirlpool.token_mint_a))
+    print('{:>20} {}'.format("token_mint_b:", whirlpool.token_mint_b))
+    print('{:>20} {}'.format("tick_spacing:", whirlpool.tick_spacing))
+    print('{:>20} {}'.format("tick_current_index:", whirlpool.tick_current_index))
+    print('{:>20} {}'.format("sqrt_price:", whirlpool.sqrt_price))
+    print('{:>20} {}'.format("price:", DecimalUtil.to_fixed(price, decimals_b)))
+
+    # calc token amounts
+    amounts = LiquidityMath.get_token_amounts_from_liquidity(
+        position.liquidity,
+        whirlpool.sqrt_price,
+        PriceMath.tick_index_to_sqrt_price_x64(position.tick_lower_index),
+        PriceMath.tick_index_to_sqrt_price_x64(position.tick_upper_index),
+        False
+    )
+    # get status
+    status = PositionUtil.get_position_status(
+        whirlpool.tick_current_index,
+        position.tick_lower_index,
+        position.tick_upper_index
+    )
+    print('{:*^80}'.format("Position"))
+    print('{:>20} {}'.format("name:", position.pubkey))
+    print('{:>20} {}'.format("address:", position.position_mint))
+    print('{:>20} {}'.format("liquidity:", position.liquidity))
+    print('{:>20} {}'.format("a_amount(u64):", amounts.token_a))
+    print('{:>20} {}'.format("b_amount(u64):", amounts.token_b))
+    print('{:>20} {}'.format("status:", status))
+
+    # input
+    input_token = whirlpool.token_mint_b  # USDC
+    input_amount = DecimalUtil.to_u64(Decimal(deposit_amount), decimals_b)  # USDC
+    acceptable_slippage = Percentage.from_fraction(int(slippage), 100)
+    # price_lower = price / 2
+    # price_upper = price * 2
+    print('{:<80}'.format("Building a quote for increasing liquidity..."))        
+    # get quote
+    quote = QuoteBuilder.increase_liquidity_by_input_token(IncreaseLiquidityQuoteParams(
+        input_token_mint=input_token,
+        input_token_amount=input_amount,
+        token_mint_a=whirlpool.token_mint_a,
+        token_mint_b=whirlpool.token_mint_b,
+        sqrt_price=whirlpool.sqrt_price,
+        tick_current_index=whirlpool.tick_current_index,
+        tick_lower_index=position.tick_lower_index,
+        tick_upper_index=position.tick_upper_index,
+        slippage_tolerance=acceptable_slippage,
+    ))
+    print('{:*^80}'.format("Quote"))
+    print('{:>20} {}'.format("liquidity:", quote.liquidity))
+    print('{:>20} {}'.format("est_token_a:", quote.token_est_a))
+    print('{:>20} {}'.format("est_token_b:", quote.token_est_b))
+    print('{:>20} {}'.format("max_token_a:", quote.token_max_a))
+    print('{:>20} {}'.format("token_max_b:", quote.token_max_b))
+    
+    # get ATA (considering WSOL)
+    token_account_a = await TokenUtil.resolve_or_create_ata(ctx.connection, ctx.wallet.pubkey(), whirlpool.token_mint_a, quote.token_max_a)
+    token_account_b = await TokenUtil.resolve_or_create_ata(ctx.connection, ctx.wallet.pubkey(), whirlpool.token_mint_b, quote.token_max_b)
+    print('{:>20} {}'.format("token_account_a:", token_account_a.pubkey))
+    print('{:>20} {}'.format("token_account_b:", token_account_b.pubkey))
+
+    # build transaction
+    tx = TransactionBuilder(ctx.connection, ctx.wallet)
+
+    # WSOL considring
+    tx.add_instruction(token_account_a.instruction)
+    tx.add_instruction(token_account_b.instruction)
+
+    # increase liquidity
+    tick_array_lower_start_tick_index = TickUtil.get_start_tick_index(position.tick_lower_index, whirlpool.tick_spacing)
+    tick_array_upper_start_tick_index = TickUtil.get_start_tick_index(position.tick_upper_index, whirlpool.tick_spacing)
+    tick_array_lower = PDAUtil.get_tick_array(ctx.program_id, whirlpool_pubkey, tick_array_lower_start_tick_index).pubkey
+    tick_array_upper = PDAUtil.get_tick_array(ctx.program_id, whirlpool_pubkey, tick_array_upper_start_tick_index).pubkey
+    position_ata = TokenUtil.derive_ata(ctx.wallet.pubkey(), position.position_mint)
+    increase_liquidity_ix = WhirlpoolIx.increase_liquidity(
+        ctx.program_id,
+        IncreaseLiquidityParams(
+            whirlpool=whirlpool_pubkey,
+            position=position_pda,
+            position_token_account=position_ata,
+            position_authority=ctx.wallet.pubkey(),
+            liquidity_amount=quote.liquidity,
+            token_max_a=quote.token_max_a,
+            token_max_b=quote.token_max_b,
+            token_owner_account_a=token_account_a.pubkey,
+            token_owner_account_b=token_account_b.pubkey,
+            token_vault_a=whirlpool.token_vault_a,
+            token_vault_b=whirlpool.token_vault_b,
+            tick_array_lower=tick_array_lower,
+            tick_array_upper=tick_array_upper,
+        )
+    )
+    tx.add_instruction(increase_liquidity_ix)
+
+    if priority_fee is not None:
+        tx.set_compute_unit_limit(200000)
+        # add priority fees (+ 1000 lamports)
+        # tx.set_compute_unit_price(int(1000 * 10**6 / 200000))
+        tx.set_compute_unit_price(int(priority_fee * 10**6 / 200000))
+    # execute
+    print('{:<80}'.format("Build and execute transaction..."))
+    signature = await tx.build_and_execute()
+    print('{:>20} {}'.format("TX signature:", signature))
