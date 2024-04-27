1edb9a33d0477c1ce9b34027b4720464e6c6413d
ricardomarquina1989 <ricardoandresmarquinamolina92@gmail.com>
2024-04-27 11:09:09 -0300

    show wallet address that user input

--- examples/utils.py
+++ examples/utils.py
@@ -13,11 +13,13 @@ from constants import *
     Read wallet from file and return the keypair.
     Select one wallet based on APP_ENV.
 """
-def get_context() -> WhirlpoolContext:   
+def get_context(other_wallet=None) -> WhirlpoolContext:   
     wallet_path = ("wallet.json", "wallet_main.json")[APP_ENV == "main"]
     with Path(wallet_path).open() as f:
         keypair = Keypair.from_bytes(bytes(json.load(f)))
-    print(f"wallet pubkey {keypair.pubkey()}")
+    
+    if other_wallet is None:
+        print(f"Wallet Pubkey {keypair.pubkey()}")
 
     connection = AsyncClient(RPC_ENDPOINT_URL)  # Create Solana RPC client
     ctx = WhirlpoolContext(ORCA_WHIRLPOOL_PROGRAM_ID, connection, keypair)  # Create Whirlpool context
